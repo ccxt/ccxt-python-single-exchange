bcd48f671579978678b6bdfdcd4b210da6a05af5
t.t <7117978+ttodua@users.noreply.github.com>
2025-03-19 01:44:54 +0400

    -

--- build/update-readme.sh
+++ build/update-readme.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Define the array of items
 my_array=("binance" "okx")
-
-# Define the marker string
 marker="### XYZ"
-
-# Define the README file path
 readme_file="$GITHUB_WORKSPACE/README.md"
 
-# Function to generate the list items
 generate_list_items() {
-  local array=("${!1}") #take array as argument
+  local array=("${!1}")
   local repo_prefix="https://github.com/my/"
 
   for item in "${array[@]}"; do
@@ -19,19 +13,17 @@ generate_list_items() {
   done
 }
 
-# Check if the README file exists
-if [ ! -f "$readme_file" ]; then
-  echo "Error: README.md not found."
-  exit 1
-fi
+# Generate the list items and store them in a variable
+list_items=$(generate_list_items my_array)
 
-# Read the README file and remove everything after the marker
-content=$(sed "/$marker/q" "$readme_file")
-
-# Append the marker and the new list items
-new_content="$content\n$marker\n$(generate_list_items my_array)\n"
-
-# Write the new content to the README file
-echo "$new_content" > "$readme_file"
+awk -v marker="$marker" -v list_items="$list_items" '
+  $0 == marker {
+    found = 1;
+    print marker; # Print the marker
+    print list_items; # Print the list items
+    next;
+  }
+  !found { print }
+' "$readme_file" > "$readme_file.tmp" && mv "$readme_file.tmp" "$readme_file"
 
 echo "README.md updated successfully."
